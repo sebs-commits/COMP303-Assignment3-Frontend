bc5888aa034aa704a02c71e0dd98f41d8d6282c0
Sebastian <sebastian_bdavid@hotmail.com>
2025-03-21 19:00:11 -0400

    init project

---
+++ src/main/java/com/example/sebastiandavid_301241956_assignment3_frontend/SebastianDavid301241956Assignment3FrontendApplication.java
@@ -0,0 +1,13 @@
+package com.example.sebastiandavid_301241956_assignment3_frontend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SebastianDavid301241956Assignment3FrontendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SebastianDavid301241956Assignment3FrontendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/sebastiandavid_301241956_assignment3_frontend/SebastianDavid301241956Assignment3FrontendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sebastiandavid_301241956_assignment3_frontend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SebastianDavid301241956Assignment3FrontendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
